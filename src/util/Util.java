4c9b94a456d638bf4fb5dd84d6939b4fe80a8798
natalucas <nata.santos@estudante.ifms.edu.br>
2025-09-22 21:03:08 -0300

    criação do util

---
+++ src/util/Util.java
@@ -0,0 +1,93 @@
+package tools;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Natã
+ */
+public class Util {
+
+    public static void habilitar(boolean valor, JComponent... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            componentes[i].setEnabled(valor);
+        }
+    }
+
+    public static void limpar(JComponent... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            //instanceof
+            if (componentes[i] instanceof JTextField) {
+                ((JTextField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JComboBox) {
+                ((JComboBox) componentes[i]).setSelectedIndex(-1);
+            } else if (componentes[i] instanceof JFormattedTextField) {
+                ((JFormattedTextField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JCheckBox) {
+                ((JCheckBox) componentes[i]).setSelected(false);
+            } else if (componentes[i] instanceof JPasswordField) {
+                ((JPasswordField) componentes[i]).setText("");
+            }
+        }
+    }
+
+    public static void mensagem(String cad) {
+        JOptionPane.showMessageDialog(null, cad);
+    }
+
+    public static boolean perguntar(String cad) {
+            JOptionPane.showConfirmDialog(null,cad,"", JOptionPane.YES_NO_OPTION);
+        return true;
+    }
+
+    public static int strToInt(String num) {
+        return Integer.valueOf(num);
+    }
+
+    public static String intToStr(int num) {
+        return String.valueOf(num);
+    }
+
+    public static double strToDouble(String cad) {
+        try {
+            return Double.parseDouble(cad.replace(",", "."));
+        } catch (NumberFormatException e) {
+            mensagem("Erro");
+            return 0.0;
+        }
+    }
+
+    public static String doubleToStr(String num) {
+        return String.valueOf(num);
+    }
+
+    public static Date strToDate(String data) {
+        SimpleDateFormat datando = new SimpleDateFormat("dd/MM/yyyy");
+        datando.setLenient(false);
+        try {
+            return datando.parse(data);
+        } catch (ParseException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static String dateToStr(Date data) {
+        if (data == null) {
+            return "";
+        }
+        SimpleDateFormat datando = new SimpleDateFormat("dd/MM/yyyy");
+        return datando.format(data);
+    }
+}
